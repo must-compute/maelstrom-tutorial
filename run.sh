f2d9e6c7a8fa00c6d5cd490f9c11352e4c736168
must-compute <72048854+must-compute@users.noreply.github.com>
2025-01-09 15:14:56 +0000

    update runner script

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [ "$1" == "broadcast" ]; then
-  cargo build && ./maelstrom/maelstrom test -w broadcast --bin ./target/debug/maelstrom-tutorial --time-limit 5 --rate 10
+MAELSTROM="./maelstrom/maelstrom"
+BINARY="./target/debug/maelstrom-tutorial"
+
+if [ "$1" == "broadcast-2s" ]; then
+  cargo build && $MAELSTROM test -w broadcast --bin $BINARY --time-limit 2 --rate 10
+elif [ "$1" = "broadcast-20s" ]; then
+  cargo build && $MAELSTROM test -w broadcast --bin $BINARY --time-limit 20 --rate 100
 else
   echo "unknown command"
 fi
