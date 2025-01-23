5cd092753f7588a4476af7567c1dce9b5f18d05d
must-compute <72048854+must-compute@users.noreply.github.com>
2025-01-23 19:22:53 +0000

    remove unused code/lines in run.sh

--- run.sh
+++ run.sh
@@ -14,14 +14,6 @@ elif [ "$1" = "broadcast-20s" ]; then
 elif [ "$1" = "broadcast-20s-partition" ]; then
   cargo build && $MAELSTROM test -w broadcast --bin $BINARY --time-limit 20 --nemesis partition --topology tree4 --log-stderr
 elif [ "$1" = "loop" ]; then
-  # while true; do
-      # cargo build && $MAELSTROM test -w broadcast --bin $BINARY --time-limit 2 --log-stderr | tee >(if [[ $(tail -n 1) == *"good"* ]]; then
-      #     echo "Build failed!"
-      #     exit 0
-      # fi)
-      # echo "Build succeeded, running again..."
-  # done
-
   for i in {1..100}; do
       echo "Attempt $i"
       output=$(cargo build && $MAELSTROM test -w broadcast --bin $BINARY --time-limit 20 --nemesis partition --topology tree4 --log-stderr )
@@ -35,10 +27,7 @@ elif [ "$1" = "loop" ]; then
           exit 1
       fi
   done
-
   echo "All attempts completed successfully"
-
-  
 
 else
   echo "unknown command"
